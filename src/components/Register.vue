c6f4a0877b4c59bca2f2619bca9a937f40527af7
Vlad Stepanenko <vladyslav.stepanenko@gmail.com>
2018-08-08 00:48:28 +0300

    Pretty up register form

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,14 +1,28 @@
 <template>
-    <div>
-        <h1>Register</h1>
-        <input type="email" name="email" placeholder="E-mail" v-model="email" />
-        <input type="text" name="username" placeholder="Username" v-model="username" />
-        <input type="password" name="password" placeholder="Password" v-model="password" />
-        <div class="error">
-            <p v-for="error in errors" :key="error.message">{{error.message}}</p>
-        </div>
-        <button @click="register">Register</button>
-    </div>
+    <v-app>
+        <v-content>
+            <v-container fluid fill-height>
+                <v-layout column align-center justify-center>
+                    <v-flex xs12 sm8 md4>
+                        <div class="white elevation-2">
+                            <v-toolbar flat dense dark class="green">
+                                <v-toolbar-title>Register</v-toolbar-title>
+                            </v-toolbar>
+                            <div class="pl-4 pr-4 pt-2 pb-2 text-xs-center">
+                                <v-text-field type="email" name="email" placeholder="E-mail" v-model="email"></v-text-field>
+                                <v-text-field type="text" name="username" placeholder="Username" v-model="username"></v-text-field>
+                                <v-text-field type="password" name="password" placeholder="Password" v-model="password"></v-text-field>
+                                <div class="error">
+                                    <p v-for="error in errors" :key="error.message">{{error.message}}</p>
+                                </div>
+                                <v-btn class="green" @click="register">Register</v-btn>
+                            </div>
+                        </div>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
